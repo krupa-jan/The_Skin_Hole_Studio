0a365aa7e83145b0560c4dc8c8efd529e04c4def
krupa-jan <132360261+krupa-jan@users.noreply.github.com>
2025-11-10 19:47:31 +0100

    Nav: Přidán div pro lepší strukturu

--- includes/nav.php
+++ includes/nav.php
@@ -1,17 +1,18 @@
 <nav>
-    <div>
-        <a href="index.php">
-            <img src="assets/images/logo/Logo_500x500_Transparent.webp" alt="Logo The Skin Hole Studio" class="nav-logo">
-        </a>
-    </div>
+    <div class="nav-container">
+        <div>
+            <a href="index.php">
+                <img src="assets/images/logo/Logo_500x500_Transparent.webp" alt="Logo The Skin Hole Studio" class="nav-logo">
+            </a>
+        </div>
 
-    <div class="nav-space"></div>
+        <div class="nav-space"></div>
     
-    <div>
-        <a href="index.html">Home</a>
-        <a href="index.html">Odkaz1</a>
-        <a href="index.html">Odkaz2</a>
-        <a href="index.html">Odkaz3</a>
+        <div>
+            <a href="index.html">Home</a>
+            <a href="index.html">Odkaz1</a>
+            <a href="index.html">Odkaz2</a>
+            <a href="index.html">Odkaz3</a>
+        </div>
     </div>
-
 </nav>
