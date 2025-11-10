e9c3ff826b2898115f647ddd20781322253dd429
krupa-jan <132360261+krupa-jan@users.noreply.github.com>
2025-11-10 17:32:02 +0100

    Nav: přidán základní nav
    - Přidána základní struktura souborů a složek
    - Přidány soubory .php pro navigaci

---
+++ includes/nav.php
@@ -0,0 +1,8 @@
+<nav>
+    <div>
+        <a href="index.html">Home</a>
+        <a href="index.html">Odkaz1</a>
+        <a href="index.html">Odkaz2</a>
+        <a href="index.html">Odkaz3</a>
+    </div>
+</nav>
