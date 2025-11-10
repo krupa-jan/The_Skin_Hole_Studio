da7f33a97bbf05b28d7a399a4ebd8b6a2f2526bb
krupa-jan <132360261+krupa-jan@users.noreply.github.com>
2025-11-10 19:42:35 +0100

    Style.css: přidán soubor s hlavními styly - odstranění padding a margin

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>The Skin Hole Studio</title>
 
+    <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href="assets/css/nav.css">
 </head>
 <body>
