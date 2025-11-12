825c753f623bd1b6f46f404fc2b8c186329d3479
normalnijmeno <petr.amm2100@seznam.cz>
2025-11-12 19:06:03 +0100

    CSS: propojeno o-nas.css s o-nas.php

--- o-nas.php
+++ o-nas.php
@@ -1,1 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>The Skin Hole Studio</title>
 
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/nav.css">
+    <link rel="stylesheet" href="assets/css/o-nas.css">
+</head>
+<body>
+    <header>
+        <!--Nav-->
+        <?php require 'includes/nav.php'; ?>
+    </header>
+
+    <!--Footer-->
+    <?php require 'includes/footer.php'; ?>
+</body>
+</html>
