1e4578afc797f2419514775d393d5d962b08d9e4
krupa-jan <132360261+krupa-jan@users.noreply.github.com>
2025-11-13 18:34:25 +0100

    Tlačítka: změna barev

--- includes/nav.php
+++ includes/nav.php
@@ -11,13 +11,13 @@
             <li><a href="index.html">Odkaz1</a></li>
             <li><a href="index.html">Odkaz2</a></li>
             <li><a href="index.html">Odkaz3</a></li>
-            <li class="hamburger-button" onclick=showHamburgerNav(event)><a href="#"><svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="#e3e3e3"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg></li>
+            <li class="hamburger-button" onclick=showHamburgerNav(event)><a href="#"><svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="rgb(46, 42, 57)"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg></li>
         </ul>
 
     </div>
 
     <ul class="hamburger-nav">
-            <li class="close-button" onclick=hideHamburgerNav(event)><a href="#"><svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="#e3e3e3"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg></li>
+            <li class="close-button" onclick=hideHamburgerNav(event)><a href="#"><svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="rgb(46, 42, 57)"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg></li>
             <li><a href="index.html">Odkaz0</a></li>
             <li><a href="index.html">Odkaz1</a></li>
             <li><a href="index.html">Odkaz2</a></li>
