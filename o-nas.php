303ef5cf7c81ec1726afa11993bf5052b20e6344
normalnijmeno <petr.amm2100@seznam.cz>
2025-11-23 19:30:50 +0100

    Přidán text o nás a odstavec s kontakt

--- o-nas.php
+++ o-nas.php
@@ -17,6 +17,16 @@
 <div class="o-nas-hlava">
     <h1>O nás</h1>
 </div>
+<div class="o-nas-text">
+    <h2>Kdo jsme?</h2>
+    <p>The Skin Hole Studio je tetovací a piercingové studio sídlící v srdci Frýdku-Místku. Naším cílem je poskytovat kvalitní služby v oblasti tetování a piercingu, které splňují nejvyšší standardy hygieny a bezpečnosti. Náš tým tvoří zkušení a talentovaní umělci, kteří jsou vášniví pro své řemeslo a neustále se snaží zlepšovat své dovednosti.</p>
+</div>
+<div class="o-nas-kontakt">
+    <h2>Kontakt</h2>
+    <p><strong>Email:</strong></p>
+    <p><strong>Telefon:</strong></p>
+    <p><strong>Adresa:</strong></p>
+</div>
     <!--Footer-->
     <?php require 'includes/footer.php'; ?>
 </body>
