ac5dd96c16b522eb5f120058ac0b3ff0330bcc36
krupa-jan <132360261+krupa-jan@users.noreply.github.com>
2025-11-24 20:34:32 +0100

    Nav: Oprava vycentrování loga

--- includes/nav.php
+++ includes/nav.php
@@ -1,10 +1,9 @@
 <nav>
     <div class="nav-container">
-        <div>
-            <a href="#">
-                <img src="assets/images/logo/Logo_500x500_Transparent.webp" alt="Logo The Skin Hole Studio" class="nav-logo">
-            </a>
-        </div>
+
+        <a href="index.php" class="logo-a">
+            <img src="assets/images/logo/Logo_500x500_Transparent.webp" alt="Logo The Skin Hole Studio" class="logo">
+        </a>
 
         <ul class="desktop-nav">
             <li><a href="index.php">Domů</a></li>
@@ -16,6 +15,7 @@
             <li class="hamburger-button" onclick=showHamburgerNav(event)><a href="#"><svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="rgb(46, 42, 57)"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg></a></li>
             <li class="close-button" onclick=hideHamburgerNav(event)><a href="#"><svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="rgb(46, 42, 57)"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg></a></li>
         </ul>
+
     </div>
 
     <ul class="hamburger-nav">
@@ -28,6 +28,7 @@
     </ul>
 
     <div class="overlay"></div>
+    
 </nav>
 
 <script src="assets/js/nav.js"></script>
