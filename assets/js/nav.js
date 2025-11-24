adcb38c9fc2736040c861adabd808ddf82c26360
krupa-jan <132360261+krupa-jan@users.noreply.github.com>
2025-11-24 18:59:03 +0100

    Nav: Upravena responzivnost a funkčnost tlačítek

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -2,12 +2,22 @@ function showHamburgerNav(event) {
     event.preventDefault();
 
     const hamburgerNav = document.querySelector('.hamburger-nav');
+    const hamburgerButton = document.querySelector('.hamburger-button');
+    const closeButton = document.querySelector('.close-button');
+
     hamburgerNav.style.display = 'flex';
+    hamburgerButton.style.display = 'none';
+    closeButton.style.display = 'block';
 }
 
 function hideHamburgerNav(event) {
     event.preventDefault();
 
     const hamburgerNav = document.querySelector('.hamburger-nav');
+    const hamburgerButton = document.querySelector('.hamburger-button');
+    const closeButton = document.querySelector('.close-button');
+
     hamburgerNav.style.display = 'none';
+    hamburgerButton.style.display = 'block';
+    closeButton.style.display = 'none';
 }
--- includes/nav.php
+++ includes/nav.php
@@ -1,7 +1,7 @@
 <nav>
     <div class="nav-container">
         <div>
-            <a href="index.php">
+            <a href="#">
                 <img src="assets/images/logo/Logo_500x500_Transparent.webp" alt="Logo The Skin Hole Studio" class="nav-logo">
             </a>
         </div>
@@ -11,13 +11,13 @@
             <li><a href="index.html">Odkaz1</a></li>
             <li><a href="index.html">Odkaz2</a></li>
             <li><a href="index.html">Odkaz3</a></li>
-            <li class="hamburger-button" onclick=showHamburgerNav(event)><a href="#"><svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="rgb(46, 42, 57)"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg></li>
+            <li class="hamburger-button" onclick=showHamburgerNav(event)><a href="#"><svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="rgb(46, 42, 57)"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg></a></li>
+            <li class="close-button" onclick=hideHamburgerNav(event)><a href="#"><svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="rgb(46, 42, 57)"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg></a></li>
         </ul>
 
     </div>
 
     <ul class="hamburger-nav">
-            <li class="close-button" onclick=hideHamburgerNav(event)><a href="#"><svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="rgb(46, 42, 57)"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg></li>
             <li><a href="index.html">Odkaz0</a></li>
             <li><a href="index.html">Odkaz1</a></li>
             <li><a href="index.html">Odkaz2</a></li>
