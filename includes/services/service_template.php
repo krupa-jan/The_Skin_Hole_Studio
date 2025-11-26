73407c5dc747bbe4f9e1d6d4a175e70ba1406695
krupa-jan <132360261+krupa-jan@users.noreply.github.com>
2025-11-26 12:21:47 +0100

    Services: Přidána stránka služby a jejich template

---
+++ includes/services/service_template.php
@@ -0,0 +1,5 @@
+<div class="product-card">
+    <h2 class="product-title"><?php echo $name; ?></h2>
+    <p class="product-desc"><?php echo $image; ?></p>
+    <button>Koupit</button>
+</div>
---
+++ sluzby.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <title>Služby | The Skin Hole Studio</title>
+    <?php require 'includes/html_head.php'; ?>
+</head>
+
+<body>
+    <?php require 'includes/nav.php'; ?>
+    
+    <div class="services">
+        <?php
+    
+        $services = [
+            ["name" => "Služba1", "image" => ""],
+            ["name" => "Služba2", "image" => ""],
+            
+        ];
+
+        foreach ($services as $service) {
+
+            $name = $service['name'];
+            $image = $service['image'];
+        
+            include 'includes/services/service_template.php'; 
+        }
+    
+        ?>
+    </div>
+    
+    <?php require 'includes/footer.php'; ?>
+</body>
+
+</html>
