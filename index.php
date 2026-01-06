f78e607c27554b260da7698c7a97f99ca98032b3
krupa-jan <132360261+krupa-jan@users.noreply.github.com>
2026-01-06 12:11:29 +0100

    Background: Upraveno pozadí, chyba zvětšení prohlížeče

--- index.php
+++ index.php
@@ -12,7 +12,10 @@
     <?php require 'includes/nav.php'; ?>
 
     <div class="top-section">
-        <img src="/assets/images/logo/logo.svg" class="top-section-logo">
+        <img src="/assets/images/example_images/store-example.jpg" class="top-section-bg" draggable="false">
+        <div class="top-section-overlay"></div>
+
+        <img src="/assets/images/logo/logo.svg" class="top-section-logo" draggable="false">
 
         <div class="top-section-cta">
             <a href="#" class="cta-button-primary">Rezervovat termín</a>
