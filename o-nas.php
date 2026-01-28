a2edbf72c203221833fa44d03d9e42160eab5d99
Jan Krupa <132360261+krupa-jan@users.noreply.github.com>
2026-01-28 21:47:06 +0100

    O nás: přidána nová verze
    * Create o-nas.php
    
    * CSS: přidání souboru o-nas.css
    
    * CSS: propojeno o-nas.css s o-nas.php
    
    * Soubory: Přidán soubor o-nas.css, o-nas.php
    
    * přidání hlavy o-nás
    
    * Přidán text o nás a odstavec s kontakt
    
    * Vytvořené zobrazení pro mobil i PC, přidání nadpisu, textu a mista pro obrázky
    
    * Upravený vzhled celé stránky, velikosti textů, přidané obrázky

--- o-nas.php
+++ o-nas.php
@@ -14,7 +14,38 @@
         <!--Nav-->
         <?php require 'includes/nav.php'; ?>
     </header>
-
+<div class="uvodni-obrazek-wrapper">
+    <img src="https://www.bunteleute.com/files/bilder/neues-tattoostudio/tattooraum.jpeg" alt="">
+</div>
+<div class="o-nas-hlava">
+    <h1>O Nás</h1>
+</div>
+<div class="o-nas-1">
+    <h2>Nadpis 1</h2>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/a/a3/Image-not-found.png">
+    <p>Libertate concipiam aliquanto ad vereorque se ac. Nihilque meliores vel generali spectant sae speranda cui supponam. Debiliora spontaneo praeditis suspicari ea at. Fit quovis melius rum statim iis indere eas nomine. Facultates</p>
+</div>
+<div class="o-nas-2">
+    <h2>Nadpis 2</h2>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/a/a3/Image-not-found.png">
+    <p>Opinionis calescere se mentemque immittant ne animalium productus de in. Rationi petebat punctum rotunda re co conatus aliquis ac. Contingit continent veritatem opinionem in de meditatio. Re liberet eo positis limites co im</p>
+</div>
+<div class="o-nas-3">
+    <h2>Nadpis 3</h2>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/a/a3/Image-not-found.png">
+    <p>Opinionis calescere se mentemque immittant ne animalium productus de in. Rationi petebat punctum rotunda re co conatus aliquis ac. Contingit continent veritatem opinionem in de meditatio. Re liberet eo positis limites co im</p>
+</div>
+<div class="o-nas-4">
+    <h2>Nadpis 4</h2>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/a/a3/Image-not-found.png">
+    <p>Opinionis calescere se mentemque immittant ne animalium productus de in. Rationi petebat punctum rotunda re co conatus aliquis ac. Contingit continent veritatem opinionem in de meditatio. Re liberet eo positis limites co im</p>
+</div>
+<div class="o-nas-kontakt">
+    <h2>Kontakt</h2>
+    <p><strong>Email: email@gmail.com</strong></p>
+    <p><strong>Telefon: 773 938 923</strong></p>
+    <p><strong>Adresa: Ostrava 23</strong></p>
+</div>
     <!--Footer-->
     <?php require 'includes/footer.php'; ?>
 </body>
