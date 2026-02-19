3ab73dae7bdfffcf5f9947225773b06a35a469fc
Jan Krupa <132360261+krupa-jan@users.noreply.github.com>
2026-02-19 09:51:05 +0100

    O nás: Úprava odsazení

--- o-nas.php
+++ o-nas.php
@@ -11,36 +11,33 @@
 <body>
     <?php require 'includes/nav.php'; ?>
 
-    <div class="uvodni-obrazek-wrapper">
-        <img src="https://www.bunteleute.com/files/bilder/neues-tattoostudio/tattooraum.jpeg" alt="">
-    </div>
-
     <div class="o-nas-hlava">
         <h1>O Nás</h1>
     </div>
+    <div class="sekce">
+        <div class="o-nas-1">
+            <h2>Nadpis 1</h2>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/a/a3/Image-not-found.png">
+            <p>Libertate concipiam aliquanto ad vereorque se ac. Nihilque meliores vel generali spectant sae speranda cui supponam. Debiliora spontaneo praeditis suspicari ea at. Fit quovis melius rum statim iis indere eas nomine. Facultates</p>
+        </div>
 
-    <div class="o-nas-1">
-        <h2>Nadpis 1</h2>
-        <img src="https://upload.wikimedia.org/wikipedia/commons/a/a3/Image-not-found.png">
-        <p>Libertate concipiam aliquanto ad vereorque se ac. Nihilque meliores vel generali spectant sae speranda cui supponam. Debiliora spontaneo praeditis suspicari ea at. Fit quovis melius rum statim iis indere eas nomine. Facultates</p>
-    </div>
+        <div class="o-nas-2">
+            <h2>Nadpis 2</h2>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/a/a3/Image-not-found.png">
+            <p>Opinionis calescere se mentemque immittant ne animalium productus de in. Rationi petebat punctum rotunda re co conatus aliquis ac. Contingit continent veritatem opinionem in de meditatio. Re liberet eo positis limites co im</p>
+        </div>
 
-    <div class="o-nas-2">
-        <h2>Nadpis 2</h2>
-        <img src="https://upload.wikimedia.org/wikipedia/commons/a/a3/Image-not-found.png">
-        <p>Opinionis calescere se mentemque immittant ne animalium productus de in. Rationi petebat punctum rotunda re co conatus aliquis ac. Contingit continent veritatem opinionem in de meditatio. Re liberet eo positis limites co im</p>
-    </div>
+        <div class="o-nas-3">
+            <h2>Nadpis 3</h2>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/a/a3/Image-not-found.png">
+            <p>Opinionis calescere se mentemque immittant ne animalium productus de in. Rationi petebat punctum rotunda re co conatus aliquis ac. Contingit continent veritatem opinionem in de meditatio. Re liberet eo positis limites co im</p>
+        </div>
 
-    <div class="o-nas-3">
-        <h2>Nadpis 3</h2>
-        <img src="https://upload.wikimedia.org/wikipedia/commons/a/a3/Image-not-found.png">
-        <p>Opinionis calescere se mentemque immittant ne animalium productus de in. Rationi petebat punctum rotunda re co conatus aliquis ac. Contingit continent veritatem opinionem in de meditatio. Re liberet eo positis limites co im</p>
-    </div>
-
-    <div class="o-nas-4">
-        <h2>Nadpis 4</h2>
-        <img src="https://upload.wikimedia.org/wikipedia/commons/a/a3/Image-not-found.png">
-        <p>Opinionis calescere se mentemque immittant ne animalium productus de in. Rationi petebat punctum rotunda re co conatus aliquis ac. Contingit continent veritatem opinionem in de meditatio. Re liberet eo positis limites co im</p>
+        <div class="o-nas-4">
+            <h2>Nadpis 4</h2>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/a/a3/Image-not-found.png">
+            <p>Opinionis calescere se mentemque immittant ne animalium productus de in. Rationi petebat punctum rotunda re co conatus aliquis ac. Contingit continent veritatem opinionem in de meditatio. Re liberet eo positis limites co im</p>
+        </div>
     </div>
     
     <div class="o-nas-kontakt">
